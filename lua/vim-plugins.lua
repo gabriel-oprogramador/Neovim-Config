5f98fb799f15b635581f8e65a7d52a9584397c97
Gabriel Oliveira Prudente <gabrieldj.leg@gmail.com>
2024-11-05 06:28:58 -0300

    Update vim-plugins.lua

--- lua/vim-plugins.lua
+++ lua/vim-plugins.lua
@@ -2,3 +2,17 @@ require("plugs.gruvbox-config")
 require("plugs.airline-config")
 require("plugs.mason-config")
 require("plugs.lsp-config")
+require('telescope').setup {
+    defaults = {
+      file_ignore_patterns = {
+        "%.git",
+        "%.cache",
+        "%.png",
+        "%.jpg",
+        "%.jpeg",
+        "%.o",
+        ".cache",
+        "Build"
+        },
+    },
+}
